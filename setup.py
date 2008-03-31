19be6c0888251040d5d11665d3309596436a1174
David Glick <dglick@gmail.com>
2008-03-31 06:53:16 +0000

    preparation for v0.3 release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.3dev'
+version = '0.3'
 
 long_description = (
     read('README.txt')
