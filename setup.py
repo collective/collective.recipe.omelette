c13736c109e083d51c56a88e46a1e0c4ec5000fd
David Glick <dglick@gmail.com>
2008-05-29 07:17:39 +0000

    version bump -- back to dev

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.5'
+version = '0.6'
 
 long_description = (
     read('README.txt')
