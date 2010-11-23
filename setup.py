a77ada006730491efb5f7dcc02a71e1ec414634b
David Glick <dglick@gmail.com>
2010-11-23 06:57:43 +0000

    Prepare collective.recipe.omelette 0.10.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ entry_points = {"zc.buildout": ["default = %s" % entry_point],
                 "zc.buildout.uninstall": ["default = %s" % uninstall_entry_point]}
 
 install_requires=['setuptools', 'zc.buildout', 'zc.recipe.egg']
-tests_require=['zope.testing', 'zc.buildout>=1.2.1', 'zc.recipe.egg']
+tests_require=['zope.testing', 'zc.buildout>=1.5.2', 'zc.recipe.egg']
 
 setup(name='collective.recipe.omelette',
       version=version,
