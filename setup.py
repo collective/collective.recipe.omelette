7f65dc6464f0f34042184e8b2f27ef9cf78f36c1
David Glick <dglick@gmail.com>
2012-04-14 18:55:45 -0700

    vb

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.13'
+version = '0.14dev'
 
 long_description = (
     'Detailed Documentation\n'
