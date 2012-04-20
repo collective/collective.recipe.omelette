278e325127226a907a631767517abd72504a195a
Roman Lacko <backup.rlacko@gmail.com>
2012-04-20 10:51:14 +0200

    fix unlink

--- collective/recipe/omelette/utils.py
+++ collective/recipe/omelette/utils.py
@@ -7,17 +7,20 @@ if sys.platform[:3].lower() == "win":
 if WIN32:
     from jaraco.windows import filesystem
     
+    def unlink(path):
+        if not filesystem.islink(path):
+            return
+        os.rmdir(path)
+    
     symlink = filesystem.symlink
     islink = filesystem.islink
-    unlink = os.rmdir
     
     def rmtree(location, nonlinks=True):
         # Explicitly unlink all junction'd links
         for root, dirs, files in os.walk(location, topdown=False):
             for dir in dirs:
                 path = os.path.join(root, dir)
-                if islink(path):
-                    unlink(path)
+                unlink(path)
         # Then get rid of everything else
         if nonlinks:
             shutil.rmtree(location)
