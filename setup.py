4264b5073207e862cdb9e037f81f15f49721e1ed
Christian Klinger <ck@novareto.de>
2021-11-28 11:12:33 +0100

    black style formatting

--- setup.py
+++ setup.py
@@ -6,66 +6,61 @@ import os
 import sys
 from setuptools import setup, find_packages
 
+
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0.1.dev0'
+
+version = "1.0.1.dev0"
 
 long_description = (
-    'Detailed Documentation\n'
-    '**********************\n'
-    + '\n' +
-    read('README.rst')
-    + '\n' +
-    'Change history\n'
-    '**************\n'
-    + '\n' +
-    read('CHANGES.txt')
-    + '\n' +
-    'Contributors\n'
-    '************\n'
-    + '\n' +
-    read('CONTRIBUTORS.txt')
-    )
-entry_point = 'collective.recipe.omelette:Recipe'
-uninstall_entry_point = 'collective.recipe.omelette:uninstall'
+    "Detailed Documentation\n"
+    "**********************\n" + "\n" + read("README.rst") + "\n" + "Change history\n"
+    "**************\n" + "\n" + read("CHANGES.txt") + "\n" + "Contributors\n"
+    "************\n" + "\n" + read("CONTRIBUTORS.txt")
+)
+entry_point = "collective.recipe.omelette:Recipe"
+uninstall_entry_point = "collective.recipe.omelette:uninstall"
 
-entry_points = {"zc.buildout": ["default = %s" % entry_point],
-                "zc.buildout.uninstall": ["default = %s" % uninstall_entry_point]}
+entry_points = {
+    "zc.buildout": ["default = %s" % entry_point],
+    "zc.buildout.uninstall": ["default = %s" % uninstall_entry_point],
+}
 
-install_requires=['setuptools', 'zc.buildout', 'zc.recipe.egg']
-tests_require=['zope.testing', 'zc.buildout[test]', 'zc.recipe.egg', 'pytest']
+install_requires = ["setuptools", "zc.buildout", "zc.recipe.egg"]
+tests_require = ["zope.testing", "zc.buildout[test]", "zc.recipe.egg", "pytest"]
 
 if sys.platform[:3].lower() == "win":
     install_requires += ["ntfsutils;python_version<'3.0'"]
 
-setup(name='collective.recipe.omelette',
-      version=version,
-      description="Creates a unified directory structure of installed packages, symlinking to the actual contents, in order to ease navigation.",
-      long_description=long_description,
-      classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Framework :: Buildout',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        ],
-      keywords='buildout eggs namespace',
-      author='David Glick, Groundwire',
-      author_email='davidglick@groundwire.org',
-      url='https://github.com/collective/collective.recipe.omelette',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective', 'collective.recipe'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=install_requires,
-      tests_require=tests_require,
-      extras_require=dict(tests=tests_require),
-      test_suite = 'collective.recipe.omelette.tests.test_docs.test_suite',
-      entry_points=entry_points,
-      )
+setup(
+    name="collective.recipe.omelette",
+    version=version,
+    description="Creates a unified directory structure of installed packages, symlinking to the actual contents, in order to ease navigation.",
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Buildout",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="buildout eggs namespace",
+    author="David Glick, Groundwire",
+    author_email="davidglick@groundwire.org",
+    url="https://github.com/collective/collective.recipe.omelette",
+    license="GPL",
+    packages=find_packages(exclude=["ez_setup"]),
+    namespace_packages=["collective", "collective.recipe"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require=dict(tests=tests_require),
+    test_suite="collective.recipe.omelette.tests.test_docs.test_suite",
+    entry_points=entry_points,
+)
