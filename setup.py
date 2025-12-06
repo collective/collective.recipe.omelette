3836eadda7b925b71ddeaa1d52ced16541d05b21
Gil Forcada Codinachs <gil.gnome@gmail.com>
2025-12-06 11:09:42 +0100

    Bumped version for breaking release.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = "2.0.1.dev0"
+version = "3.0.0.dev0"
 
 long_description = (
     "Detailed Documentation\n"
