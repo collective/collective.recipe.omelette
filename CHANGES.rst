0ed23e280dc2c97eb6956b31f356e13d371255af
Maurits van Rees <maurits@vanrees.org>
2025-12-09 20:52:29 +0100

    Preparing release 3.0.0a1
    [ci skip]

---
+++ CHANGES.rst
@@ -0,0 +1,8 @@
+3.0.0a1 (2025-12-09)
+--------------------
+
+Breaking changes:
+
+
+- Replace ``pkg_resources`` namespace with PEP 420 native namespace.
+  Support only Plone 6.2 and Python 3.10+. (#3928)
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = "3.0.0a1.dev0"
+version = "3.0.0a1"
 
 long_description = (
     "Detailed Documentation\n"
